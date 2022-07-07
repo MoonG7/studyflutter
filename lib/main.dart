fbf52a279f59187fa3c9fd002dd0f6358779b137
tjansry7 <tjansry7@naver.com>
2022-07-07 15:54:08 +0900

    폰트 스타일 지정

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,42 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: const MyHomePage(),
+      home: const MyCard(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyCard extends StatelessWidget {
+  const MyCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('myApp'),
+        title: const Text('studyFlutter'), //app bar tile 지정
+        centerTitle: true,
+        backgroundColor: Colors.yellow[100], //app bar 색상 지정
+        elevation: 0.0, // 앱바 그림자 지우기
       ),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
-            Text('hi'),
-            Text('hi2'),
+            Text('Hi1',
+            style: TextStyle(
+              color: Colors.blue, //폰트 색상
+              letterSpacing: 4.0, //폰트 간격
+              fontSize: 30.0, // 폰트 사이즈
+              fontWeight: FontWeight.bold // 폰트 크기
+            )),
+            SizedBox(  //텍스트 사이의 간격
+              height: 20.0,
+            ),
+            Text('Hi2')
           ],
         ),
-      ),
+      )
+
     );
   }
 }
