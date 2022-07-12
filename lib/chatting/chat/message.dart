d38bc144c7aa92b8d44371cd4b5706a1f1461146
tjansry7 <tjansry7@naver.com>
2022-07-12 15:07:34 +0900

    챗팅 버블 만들고 메시지 배치하기

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nao/chatting/chat/chat_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Messages extends StatelessWidget {
   const Messages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
@@ -20,12 +23,16 @@ class Messages extends StatelessWidget {
         }
 
         final chatDocs = snapshot.data!.docs;
+        final user = FirebaseAuth.instance.currentUser;
 
         return ListView.builder(
             reverse: true,
             itemCount: chatDocs.length,
             itemBuilder: (context, index) {
-              return Text(chatDocs[index]['text']);
+              return ChatBubble(
+                chatDocs[index]['text'],
+                chatDocs[index]['userID'].toString() == user!.uid,
+              );
             });
       },
     );
