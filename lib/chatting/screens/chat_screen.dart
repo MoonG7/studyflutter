202131290e6130e8732ec8b03c4c5338a17c1587
tjansry7 <tjansry7@naver.com>
2022-07-12 13:09:34 +0900

    파이어베이스 데이터베이스 연결 및 Stream 데이터 출력

--- lib/chatting/screens/chat_screen.dart
+++ lib/chatting/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -35,24 +36,44 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat screen'),
-        actions: [
-          IconButton(
-            onPressed: (){
-              _authentication.signOut();
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.logout,
-              color: Colors.white,
+        appBar: AppBar(
+          title: Text('Chat screen'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _authentication.signOut();
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Chat screen'),
-      ),
-    );
+          ],
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/OLfCyCc9quXO7s4fg7CN/message')
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting) { //데이터 대기
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final docs = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index){
+                return Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(docs[index]['text']),
+                );
+              },
+            );
+          },
+        ));
   }
 }
